033ddc8914368db5a3c0d678861eb8b4da19f7b0
Juan Manuel Berros <juanmaberros@gmail.com>
2016-06-28 20:42:25 -0300

    red border around off values of Nanodrop

--- app/decorators/nanodrop_quantification_decorator.rb
+++ app/decorators/nanodrop_quantification_decorator.rb
@@ -5,7 +5,14 @@ class NanodropQuantificationDecorator < Draper::Decorator
     "#{object.concentration} #{object.class::CONCENTRATION_UNIT}"
   end
 
+  # Generate a warning-highlighted table cell if the ratio 260/280 is off-range
+  def ratio_260_280_td
+    ratio_ok = object.class::RATIO_260_280_OK_RANGE.member? object.ratio_260_280
+    background_color_class = ratio_ok ? '' : 'alert-border'
+    h.content_tag :td, object.ratio_260_280, class: background_color_class
+  end
+
   def created_at
-    object.created_at.strftime("%-d %b %Y")
+    object.created_at.strftime('%-d %b %Y')
   end
 end

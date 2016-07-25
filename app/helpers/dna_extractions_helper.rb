bfcc7da461d73eeecbc7e340b02ea490a1fa357c
Juan Manuel Berros <juanmaberros@gmail.com>
2016-07-25 11:59:39 -0300

    handle no comment in the gel picture

--- app/helpers/dna_extractions_helper.rb
+++ app/helpers/dna_extractions_helper.rb
@@ -2,9 +2,11 @@ module DnaExtractionsHelper
   def extract_img_comment(img_path)
     extension = File.extname(img_path)
 
-    if %w(.jpg .jpeg .JPG .JPEG).include? extension
+    if File.exist?(img_path) && %w(.jpg .jpeg .JPG .JPEG).include?(extension)
       image = EXIFR::JPEG.new img_path
-      image.comment.gsub("\n", '<br>').html_safe
+      img_comment = image.comment && image.comment.gsub("\n", '<br>').html_safe
     end
+
+    img_comment || ''
   end
 end

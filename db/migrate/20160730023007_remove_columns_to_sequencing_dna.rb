b32455972057b3240392f6defb58458d94b41a66
Juan Manuel Berros <juanmaberros@gmail.com>
2016-08-01 12:33:46 -0300

    remove intentional Exception from migras

--- db/migrate/20160730023007_remove_columns_to_sequencing_dna.rb
+++ db/migrate/20160730023007_remove_columns_to_sequencing_dna.rb
@@ -1,12 +1,5 @@
 class RemoveColumnsToSequencingDna < ActiveRecord::Migration
   def change
-    puts
-    puts '!! Intentional Exception.'
-    puts '!! Run scripts/move_well ...'
-    puts '!! And then comment out this Exception'
-    puts
-    raise Exception
-    # ^ Remove after running the script scripts/move_well... in prod
     remove_column :dna_extractions_sequencings, :well, :string
     remove_column :dna_extractions_sequencings, :i5, :string
     remove_column :dna_extractions_sequencings, :i7, :string
